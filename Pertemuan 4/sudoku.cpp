fd6b1f5f42a6217e0ab2a1808ba0d381454ec4ae
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-10-27 22:28:20 +0700

    Pertemuan 4 : Traversal Graf

---
+++ Pertemuan 4/sudoku.cpp
@@ -0,0 +1,96 @@
+/*
+Nama: Yehezkiel Gustav Setiawan sitanggang
+NIM: 241401070
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Fungsi untuk mencetak papan Sudoku
+void printBoard(const vector<vector<int>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+            if ((j + 1) % 3 == 0) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+        if ((i + 1) % 3 == 0) {
+            cout << endl;
+        }
+    }
+}
+
+// Fungsi untuk memeriksa apakah sebuah angka valid di posisi tertentu
+bool isValid(const vector<vector<int>>& board, int row, int col, int num) {
+    // Periksa baris dan kolom
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
+    }
+
+    // Periksa sub-kotak 3x3
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Fungsi utama pemecah Sudoku menggunakan rekursif DFS
+bool solveSudoku(vector<vector<int>>& board) {
+    //tampilkan boardnya
+    for (int i = 0; i<9; i++){ 
+        for (int j = 0; j<9; j++){
+            if (board[i][j] == 0){ // cek nilai 0
+                for (int r = 1; r<=9 ; r++){
+                    if (isValid(board,i,j,r)){
+                        board[i][j] = r;//untuk masukkan angka yang sesuai dengan board
+                        if (solveSudoku(board)){
+                            return true;//jika berhasil maka board akan dikembalikan lagi
+                        } board[i][j] = 0; //jika gagal, board nya di kosongin
+                    }
+                }  return false; //jika tidak ada lagi angka yang sesuai
+            } 
+        } 
+    }
+    return true; //jika tidak ada lagi sel yang kosong
+       
+}
+
+int main() {
+    vector<vector<int>> board = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+    
+
+    cout << "Papan Sudoku awal:" << endl;
+    printBoard(board);
+
+    if (solveSudoku(board)) {
+        cout << "Papan Sudoku terpecahkan:" << endl;
+        printBoard(board);
+    } else {
+        cout << "Tidak ada solusi yang ditemukan." << endl;
+    }
+
+    return 0;
+}
